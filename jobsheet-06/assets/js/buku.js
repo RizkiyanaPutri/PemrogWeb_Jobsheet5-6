e7e26bb5404feb93ed21d26621b02a4237b0a4d6
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2026-09-23 18:06:56 +0700

    Jobsheet 6 8.4 No.2

--- jobsheet-06/assets/js/buku.js
+++ jobsheet-06/assets/js/buku.js
@@ -1,3 +1,4 @@
-  document.addEventListener("DOMContentLoaded", function () {
-      muatDaftarData("../data/buku.json", ["judul", "pengarang", "tahun", "stok"]);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    muatDaftarData("../data/buku.json", ["judul", "pengarang", "tahun", "stok", "kategori"]);
+});
+ 
